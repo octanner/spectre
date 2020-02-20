748edd7d1498aa7125d24db916cd584e679229bc
Lindsey Johnson <LindseyJJ@users.noreply.github.com>
2020-02-20 13:14:32 -0700

    a;ldk

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,8 +24,11 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   config.assets.css_compressor = :sass
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # fallback to assets pipeline if a precompiled asset is missed.
+  config.cache_classes = true
+  config.serve_static_assets = true
+  config.assets.compile = true
+  config.assets.digest = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
